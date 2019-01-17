d1ef91d63ccb7e3f0fbbabc261fec60fa8783ddb
James Macharia <jamesmasher13@gmail.com>
2019-01-17 16:40:50 +0300

    Add database connection

--- config/config.js
+++ config/config.js
@@ -1,2 +1,11 @@
 const dotenv = require("dotenv");
 dotenv.config();
+
+module.exports = {
+  development: {
+    DATABASE_URL: process.env.DATABASE_URL
+  },
+  testing: {
+    DATABASE_URL: process.env.DATABASE_URL_TEST
+  }
+};
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-
+const db = require("./config/config");
 const port = process.env.PORT || 5000;
+
+mongoose.connect(
+  db.development.DATABASE_URL,
+  { useNewUrlParser: true }
+);
+
+mongoose.connection
+  .once("open", () => {
+    console.log("Connection to the database successfully made ");
+  })
+  .on("error", error => {
+    console.log(error);
+  });
 
 app.listen(port, () => {
   console.log(`Server running at http://127.0.0.1:${port}`);
