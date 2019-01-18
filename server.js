fdcebedd46b674df942517eb25c16e8f80b06b70
James Macharia <jamesmasher13@gmail.com>
2019-01-18 12:19:13 +0300

    Change to es6 imports

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-const db = require("./config/config");
+import express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import db from "./config/config";
 const port = process.env.PORT || 5000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
 mongoose.connect(
   db.development.DATABASE_URL,
@@ -16,6 +22,11 @@ mongoose.connection
   .on("error", error => {
     console.log(error);
   });
+
+// Thirdparty middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`Server running at http://127.0.0.1:${port}`);
