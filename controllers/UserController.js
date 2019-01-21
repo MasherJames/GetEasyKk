bc2b52bfd0f021f5b0c0e8983d0fc41ae9f89c62
James Macharia <jamesmasher13@gmail.com>
2019-01-21 14:19:31 +0300

    Update path

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,34 +6,28 @@ import User from "../models/User";
 
 export default class UserController {
   static signup(req, res) {
-    try {
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(req.body.password, salt, (err, hash) => {
-          if (err) {
-            res.status(500).json({
-              error: {
-                message: err
-              }
-            });
-          } else {
-            const newUser = new User({
-              username: req.body.username,
-              email: req.body.email,
-              password: hash
-            });
-            newUser.save();
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(req.body.password, salt, (err, hash) => {
+        if (err) {
+          res.status(500).json({
+            error: {
+              message: err
+            }
+          });
+        } else {
+          const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hash
+          });
+          newUser.save().then(user => {
             res.status(201).json({
-              message: `Account created successfully`
+              message: `Account created successfully`,
+              user
             });
-          }
-        });
-      });
-    } catch (err) {
-      res.status(500).json({
-        error: {
-          message: err
+          });
         }
       });
-    }
+    });
   }
 }
