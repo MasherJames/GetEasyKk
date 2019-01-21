02115d35d25ff9dabf40f101c3e333fb3023d2e7
James Macharia <jamesmasher13@gmail.com>
2019-01-21 14:19:12 +0300

    Add utils

---
+++ utils/middlewares/userValidator.js
@@ -0,0 +1,26 @@
+import User from "../../models/User";
+import registerValidator from "../validators/register";
+
+export default class UserValidator {
+  static correctCreds(req, res, next) {
+    const { errors, isValid } = registerValidator(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+    next();
+  }
+
+  static uniqueUser(req, res, next) {
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (user) {
+          return res.status(409).json({
+            message: "User with this email already exists"
+          });
+        }
+        next();
+      })
+      .catch(next);
+  }
+}
