6e22cc99f5cff27cf8d84c019e6304d4115e1540
James Macharia <jamesmasher13@gmail.com>
2019-01-22 17:32:39 +0300

    Update rides

--- controllers/RideController.js
+++ controllers/RideController.js
@@ -32,6 +32,11 @@ export default class RideController {
   static getRide(req, res) {
     Ride.findById(req.params.id)
       .then(ride => {
+        if (!ride) {
+          return res.status(404).json({
+            message: "No ride with that id"
+          });
+        }
         res.status(200).json(ride);
       })
       .catch(err =>
@@ -47,7 +52,7 @@ export default class RideController {
         .then(ride => {
           if (ride.driver.toString() !== req.user.id) {
             return res.status(401).json({
-              message: "Your are no allowed to delete this ride"
+              message: "You are no allowed to delete this ride"
             });
           }
           ride.remove().then(() =>
@@ -59,6 +64,34 @@ export default class RideController {
         .catch(err =>
           res.status(404).json({
             message: "No ride with this id"
+          })
+        );
+    });
+  }
+
+  static updateRide(req, res) {
+    User.findById(req.user.id).then(user => {
+      Ride.findById(req.params.id)
+        .then(ride => {
+          if (ride.driver.toString() !== req.user.id) {
+            return res.status(401).json({
+              message: "You are not allowed to updated this ride"
+            });
+          }
+          ride
+            .findOneAndUpdate(
+              { user: req.user.id },
+              { $set: req.body },
+              { new: true }
+            )
+            .then(ride =>
+              res.status(200).json({ message: "Ride update successfully" })
+            )
+            .catch(err => res.json(err));
+        })
+        .catch(err =>
+          res.status(404).json({
+            message: "No ride with that d"
           })
         );
     });
